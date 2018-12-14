e4d77260229270ca4be7c1388606765de398b39e
李楠 <x_linan@phfund.com.cn>
2018-12-14 13:41:50 +0800

    新增加一行输出

--- hello.java
+++ hello.java
@@ -1,6 +1,8 @@
  public class hello{
     public static void main(String[] args){
     System.out.println("hello  java world!!!!");
+	
+	System.out.println(" new add hello  java world!!!!");
     
     
 
